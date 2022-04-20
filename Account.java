81375ca59583820dae62e40a68c7ff29430034f3
Tuấn Ngọc <103101440+TuanNgoc16@users.noreply.github.com>
2022-04-20 16:23:22 +0700

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,29 +1,56 @@
 // 1.6
-public class Account {
-    String id ;
-    String name ;
-    int balance ;
-    public Account ( String a , String b ) {
-        id = a;
-        name = b ;
+
+    public class Account {
+        String id ;
+        String name ;
+        int balance ;
+        public Account ( String a , String b ) {
+            id = a;
+            name = b ;
+
+        }
+        public Account ( String a , String b , int c) {
+            id = a;
+            name = b ;
+            balance = c ;
+        }
+        public String getId (){
+            return id ;
+        }
+        public String getName (){
+            return name ;
+        }
+        public int getBalance (){
+            return balance ;
+        }
+        public int credit ( int amount ){
+            balance += amount;
+            return balance;
+        }
+        public int debit ( int amount ) {
+            if ( amount <= balance) {
+            balance -=  amount;
+
+            }
+            else {
+                System.out.println("Amount exceeded ");
+            }
+            return balance;
+        }
+        public int transferTo ( Account another , int amount){
+            if ( amount <= balance) {
+                another.balance += amount;
+                balance -= amount;
+
+            }
+            else {
+                System.out.println("Amount exceeded ");
+            }
+            return balance;
+        }
+
+        public String toString () {
+            return "Employee [ ID = " + id + "Name  " + name   + " Balance " + balance  ;
+        }
 
     }
-    public Account ( String a , String b , int c) {
-        id = a;
-        name = b ;
-        balance = c ;
-     }
-    public String getId (){
-        return id ;
-    }
-    public String getName (){
-        return name ;
-    }
-    public int getBalance (){
-        return balance ;
-    }
-    public String toString () {
-        return "Employee [ ID = " + id + "Name  " + name   + " Balance " + balance  ;
-    }
-
-}
